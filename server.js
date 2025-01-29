e9442c7a724cc91bbdd6751456e61323e13091c6
Mallikarjun722 <anandhiremath979@gmail.com>
2025-01-29 11:37:00 +0530

    added server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/ping', (req, res) => {
+    res.send('ping successful');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
