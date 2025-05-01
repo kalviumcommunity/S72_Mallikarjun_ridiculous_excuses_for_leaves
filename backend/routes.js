8d4d2f8304f02bea52b9a3d523a72d45ac68424a
Mallikarjun722 <anandhiremath979@gmail.com>
2025-05-01 21:47:22 +0530

    Added CRUD routes and Bruno docs for ridiculous excuses

--- backend/routes.js
+++ backend/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const leaveController = require('./controllers/leaveController');
+const leaveController = require('./config/controllers/leaveController');
 
 router.post('/excuses', leaveController.createExcuse);
 router.get('/excuses', leaveController.getAllExcuses);
